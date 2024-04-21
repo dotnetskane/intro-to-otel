3fbb3a75fbf8091c70dfd52a13250bda80bd6852
Peter Nylander <peter.nylander@gmail.com>
2024-04-21 17:40:22 +0200

    Add project files.

---
+++ OpenTelemetryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace OTEL1;
+
+public static class OpenTelemetryExtensions
+{
+    public static WebApplicationBuilder ConfigureOpenTelemetry(this WebApplicationBuilder builder)
+    {
+        builder.Logging.AddOpenTelemetry(logging =>
+        {
+            logging.IncludeFormattedMessage = true;
+            logging.IncludeScopes = true;
+        });
+
+        builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+
+        return builder;
+    }
+}
